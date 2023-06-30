f6cccd05be25e0b0707c922efd7a5033d3bd7561
Andreas-Hum <heyoallos@gmail.com>
2023-06-30 17:22:02 +0200

    vector checks file

---
+++ src/utils/vectorChecks.ts
@@ -0,0 +1,37 @@
+import VectorError from "../errors/VectorError";
+import { Vector } from "../vector/Vector";
+
+export {
+    vectorArrayCheck,
+    vectorShapeCheck,
+    vectorSizeCheck,
+    
+}
+
+
+
+function vectorSizeCheck(vector1: Vector, vector2: Vector): void {
+    if (vector1.size !== vector2.size) {
+        throw new VectorError("Dimension mismatch: Sizes does not match", 602, { originalVector: vector1.size, errorVector: vector2.size })
+    }
+}
+
+function vectorShapeCheck(vector1: Vector, vector2: Vector): void {
+    if (vector1.shape !== vector2.shape) {
+        throw new VectorError(`Dimension mismatch: shapes does not match`,
+            703,
+            { vectorOne_shape: vector1.shape, vectorTwo_shape: vector2.shape });
+    }
+}
+
+function vectorArrayCheck(elementToCheck: any): void {
+    if (!Array.isArray(elementToCheck)) {
+        throw new VectorError('Elements input must be an array.', 601);
+    }
+}
+
+function vectorScalarCheck(scalar: number) {
+    if (typeof scalar !== "number") {
+        throw new VectorError("Invalid Scalar for Vector Multiplication Error", 702, { invalidScalar: scalar })
+    }
+}
--- src/vector/Vector.ts
+++ src/vector/Vector.ts
@@ -5,35 +5,12 @@ import type VectorTypes from "./VectorTypes";
 // Importing a custom Error class dedicated for errors related to Vectors
 import VectorError from "../errors/VectorError";
 
+// Importing vector checks
+import { vectorArrayCheck, vectorScalarCheck, vectorSizeCheck, vectorShapeCheck } from "../utils/vectorChecks";
+
 // Importing constants;
 import { DELTA } from "../utils/constants";
 
-
-function vectorSizeCheck(vector1: Vector, vector2: Vector): void {
-    if (vector1.size !== vector2.size) {
-        throw new VectorError("Dimension mismatch: Sizes does not match", 602, { originalVector: vector1.size, errorVector: vector2.size })
-    }
-}
-
-function vectorShapeCheck(vector1: Vector, vector2: Vector): void {
-    if (vector1.shape !== vector2.shape) {
-        throw new VectorError(`Dimension mismatch: shapes does not match`,
-            703,
-            { vectorOne_shape: vector1.shape, vectorTwo_shape: vector2.shape });
-    }
-}
-
-function vectorArrayCheck(elementToCheck: any): void {
-    if (!Array.isArray(elementToCheck)) {
-        throw new VectorError('Elements input must be an array.', 601);
-    }
-}
-
-function vectorScalarCheck(scalar: number) {
-    if (typeof scalar !== "number") {
-        throw new VectorError("Invalid Scalar for Vector Multiplication Error", 702, { invalidScalar: scalar })
-    }
-}
 
 function vectorZeroError() {
 
