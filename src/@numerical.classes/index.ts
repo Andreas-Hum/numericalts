007336894f4458104e26873ba8b708e87dd02f41
Andreas <ahumme22@student.aau.dk>
2023-08-21 01:39:20 +0200

    update export statements

--- src/@numerical.classes/index.ts
+++ src/@numerical.classes/index.ts
@@ -2,3 +2,4 @@ export * from './bigint';
 export * from './number';
 export * from './testClass';
 export * from './complex'
+export * from './fraction'
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Constants } from './constants';
 export { math } from './math';
 export { Matrix } from './matrix';
 export { Fraction } from "./@numerical.classes/fraction"
+export * from "./complex"
